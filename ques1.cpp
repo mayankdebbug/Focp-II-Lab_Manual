4169d69d131d9b9bce50e6f1fa4fb9fdb87684b4
MAYANK THAKRAN <mayankthakran@MAYANKs-MacBook-Air-3.local>
2026-01-21 13:21:55 +0530

    ques 2

---
+++ ques1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    float avg;
+    avg=(a+b+c)/3;
+    cout<<"average marks"<<avg;
+}
---
+++ ques2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int r;
+    float area;
+    cout<<"enter the range of fountain in metres";
+     cin>>r;
+    area=(22/7)*(r*r);
+    cout<<"area covered by fountain is "<<area<<"m^2";
+}
